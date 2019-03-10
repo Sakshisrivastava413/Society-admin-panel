a86c7a259edc52b387e36f0836408c37f4f2e979
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-03-10 16:39:24 +0530

    changed app flow

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,15 @@
 <template>
-  <v-app v-if="isLoggedIn">
-    <v-toolbar app>
-          <v-list class="user-info">
-            <v-list-tile avatar>
-              <v-list-tile-avatar>
-                <img src="https://randomuser.me/api/portraits/men/85.jpg">
-              </v-list-tile-avatar>
-  
-              <v-list-tile-content>
-                <v-list-tile-title>John Leider</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-      <v-toolbar-title class="headline text-uppercase">
-        <span class="font-weight-light">Admin Panel</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="#"
-        target="_blank"
-      >
-        <span class="mr-2">Log Out</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <SideMenu/>
-    <router-view />
-    </v-content>
-  </v-app>
-  <div v-else>
-    <router-view />
-  </div>
+  <Login />
 </template>
 
 <script>
-import SideMenu from './components/SideMenu'
+import Login from './views/Login';
 
 export default {
   name: 'App',
   components: {
-    SideMenu
+    Login
   },
-  data () {
-    return {
-      isLoggedIn: false
-    }
-  }
 }
 </script>
-
-<style scoped>
-.theme--light.v-list {
-  background: #f5f5f5
-}
-</style>
 
