98c4dc3e8f0ecf6456f2dd19252d0830164fbaa8
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-03-10 13:44:49 +0530

    added header in login page

---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-light">Admin Panel</span>
+      </v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+<style scoped>
+.theme--light.v-list {
+  background: #f5f5f5;
+}
+.headline {
+  margin-left: 44%;
+}
+
+.login-card-conatiner {
+  margin-top: 10%;
+}
+
+.login-card-title {
+  font-size: 30px;
+  margin-left: 35%;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.input-box {
+  margin: 10px 70px;
+}
+</style>
