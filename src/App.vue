9a13d38807ef2ad6951b6d7e465a0e4ce1f51617
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-03-11 22:25:18 +0530

    corrected mouting component, also wrapped with v-app to follow vuetify rules

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
-  <Login />
+  <v-app>
+    <router-view></router-view>
+  </v-app>
 </template>
-
-<script>
-import Login from './views/Login';
-
-export default {
-  name: 'App',
-  components: {
-    Login
-  },
-}
-</script>
-
