656dd91cf13fc0885c62243ae56df28de7f10c5d
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-03-11 22:28:04 +0530

    created new view 'society' for master admin

---
+++ src/views/Society.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <Home />
+  </v-container>
+</template>
+
+<script>
+import Home from "@/components/Home";
+import Header from "@components/Header";
+export default {
+  components: {
+    Home,
+    // Header
+  }
+};
+</script>
+
